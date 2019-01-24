d6d915588e2df55b7dc68d5b4408df75b0746d25
Sean Higgins <shiggins@looker.com>
2019-01-24 22:51:48 +0000

    Update and rename single_text_simple_9.js to single_text_simple_10.js

--- single_text_simple_10.js
+++ single_text_simple_10.js
@@ -51,19 +51,20 @@ looker.plugins.visualizations.add({
 		element.innerHTML = `"<div id="foo">Ready to render!</div>"`;
 	},
 	updateAsync: function(data, element, config, queryResponse, details, doneRendering){
-		var color = config.textColor
+		var color = config.textColor;
 		element.innerHTML = html;
-		// if (config.font_align == "right") {
-		// var str = 'hello there';
-		var valueHTML = LookerCharts.Utils.htmlForCell(cell)
-  		document.getElementById('foo').innerHTML = LookerCharts.Utils.htmlForCell(cell);
+		var valueText = LookerCharts.Utils.textForCell(cell);		
+		var customHtml = `<a href='#drillmenu' target='_self' style="color:${color}">${valueText}</a>` 
+		var str = 'hello there';
+		var valueHTML = LookerCharts.Utils.htmlForCell(cell);
+		document.getElementById('foo').innerHTML = LookerCharts.Utils.htmlForCell(cell);
 		var html = `<div id="foo" style="color:${color}">${valueHTML}</div>`;
 		for(var row of data) {
 			var cell = row[queryResponse.fields.dimensions[0].name];
 			html += LookerCharts.Utils.htmlForCell(cell);
+		element.innerHTML = html;
 		}
 		
-  	// }
 		doneRendering()
 	}
 });
