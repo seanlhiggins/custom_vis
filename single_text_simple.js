710473aab82ba7cc37b975773cce59e25a474d69
Sean Higgins <shiggins@looker.com>
2019-01-25 10:33:13 +0000

    Update and rename single_text_simple_1.js to single_text_simple.js

--- single_text_simple.js
+++ single_text_simple.js
@@ -90,7 +90,7 @@ looker.plugins.visualizations.add({
 		document.getElementById('foo').style.fontSize = size + "px";
 		document.getElementById('foo').style.textAlign = config.font_align;
 		document.getElementById('foo').style.fontFamily = config.font_style;
-		document.getElementById('bar').style.fontSize = size-3 + "px";
+		document.getElementById('bar').style.fontSize = size-10 + "px";
 		document.getElementById('bar').style.textAlign = config.font_align;
 		document.getElementById('bar').style.fontFamily = config.font_style;
 		if (textInput.length > 0){
