1ae9a436b9b4be6c0d1fdcac062a755690517387
Sean Higgins <shiggins@looker.com>
2019-03-11 18:14:37 +0000

    Add files via upload

---
+++ activitygauge.js
@@ -0,0 +1,152 @@
+// Uncomment to style it like Apple Watch
+/*
+if (!Highcharts.theme) {
+    Highcharts.setOptions({
+        chart: {
+            backgroundColor: 'black'
+        },
+        colors: ['#F62366', '#9DFF02', '#0CCDD6'],
+        title: {
+            style: {
+                color: 'silver'
+            }
+        },
+        tooltip: {
+            style: {
+                color: 'silver'
+            }
+        }
+    });
+}
+// */
+
+/**
+ * In the chart render event, add icons on top of the circular shapes
+ */
+
+/*
+Looker Vis Components:
+*/
+
+looker.plugins.visualizations.add({
+    create: function(element, config){
+        element.innerHTML = "<div id="activity_container"></div>";
+    },
+    updateAsync: function(data, element, config, queryResponse, details, doneRendering){
+        var html = "";
+        var i=0
+        while (i < 3) { 
+            for(var row of data[i]) {
+                var cell = row[queryResponse.fields.dimensions[i].name];
+                html += LookerCharts.Utils.htmlForCell(cell);
+            }
+        i++;
+            }
+        element.innerHTML = html;
+        Highcharts.chart('activity_container', {
+
+    chart: {
+        type: 'solidgauge',
+        height: '110%',
+        /* events: {
+            render: renderIcons
+        } */
+    },
+
+    title: {
+        text: 'Activity',
+        style: {
+            fontSize: '24px'
+        }
+    },
+
+    tooltip: {
+        borderWidth: 0,
+        backgroundColor: 'none',
+        shadow: false,
+        style: {
+            fontSize: '16px'
+        },
+        pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>',
+        positioner: function (labelWidth) {
+            return {
+                x: (this.chart.chartWidth - labelWidth) / 2,
+                y: (this.chart.plotHeight / 2) + 15
+            };
+        }
+    },
+
+    pane: {
+        startAngle: 0,
+        endAngle: 360,
+        background: [{ // Track for Move
+            outerRadius: '112%',
+            innerRadius: '88%',
+            backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[0])
+                .setOpacity(0.3)
+                .get(),
+            borderWidth: 0
+        }, { // Track for Exercise
+            outerRadius: '87%',
+            innerRadius: '63%',
+            backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[1])
+                .setOpacity(0.3)
+                .get(),
+            borderWidth: 0
+        }, { // Track for Stand
+            outerRadius: '62%',
+            innerRadius: '38%',
+            backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[2])
+                .setOpacity(0.3)
+                .get(),
+            borderWidth: 0
+        }]
+    },
+
+    yAxis: {
+        min: 0,
+        max: 100,
+        lineWidth: 0,
+        tickPositions: []
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                enabled: false
+            },
+            linecap: 'round',
+            stickyTracking: false,
+            rounded: true
+        }
+    },
+
+    series: [{
+        name: row[queryResponse.fields.dimensions[0].name],
+        data: [{
+            color: Highcharts.getOptions().colors[0],
+            radius: '112%',
+            innerRadius: '88%',
+            y: 80
+        }]
+    }, {
+        name: row[queryResponse.fields.dimensions[1].name],
+        data: [{
+            color: Highcharts.getOptions().colors[1],
+            radius: '87%',
+            innerRadius: '63%',
+            y: 65
+        }]
+    }, {
+        name: row[queryResponse.fields.dimensions[2].name],
+        data: [{
+            color: Highcharts.getOptions().colors[2],
+            radius: '62%',
+            innerRadius: '38%',
+            y: 50
+        }]
+    }]
+});
+        doneRendering()
+    }
+});
