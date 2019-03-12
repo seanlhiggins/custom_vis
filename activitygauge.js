6c2e355ddf482fd0da7d65f2254041de337dec4b
Sean Higgins <shiggins@looker.com>
2019-03-12 15:22:54 +0000

    Update activitygauge.js

--- activitygauge.js
+++ activitygauge.js
@@ -37,8 +37,10 @@ looker.plugins.visualizations.add({
                 console.log(queryResponse);
                 console.log(queryResponse.fields);
                 console.log(queryResponse.fields.dimensions);
-                console.log(data[0][queryResponse.fields.dimensions]);
-
+                console.log(data[0][queryResponse.fields.dimensions[0]]);
+                for(let i ==0; i<2; i++){
+                console.log(LookerCharts.Utils.htmlForCell(data[i][queryResponse.fields.dimensions[i].name]))
+            }
                id = "color_" + field.name
                options[id] =
                {
