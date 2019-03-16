fd6840e15eba791bea987fc8aba620aac3829a35
Sean Higgins <shiggins@looker.com>
2019-03-16 22:32:38 +0000

    Update combo_chart.js

--- combo_chart.js
+++ combo_chart.js
@@ -39,7 +39,7 @@ looker.plugins.visualizations.add({
         var secondCell = LookerCharts.Utils.htmlForCell(data[1][queryResponse.fields.dimensions[0].name]);
         var thirdCell = LookerCharts.Utils.htmlForCell(data[2][queryResponse.fields.dimensions[0].name]);
         var fourthCell = LookerCharts.Utils.htmlForCell(data[3][queryResponse.fields.dimensions[0].name]);
-        var testmeasure = 
+
         var dimension_head = queryResponse.fields.dimensions[0].label_short;
         var measure_header_1 = queryResponse.fields.measure_like[0].label_short;
         var measure_header_2 = queryResponse.fields.measure_like[1].label_short;
@@ -52,7 +52,7 @@ looker.plugins.visualizations.add({
           return total + Math.round(num);
         }
 
-        console.log(queryResponse,data,firstCell);
+        console.log(queryResponse,data,firstCell,thirdMeasArray);
        
         element.innerHTML = html;
         var container = element.appendChild(document.createElement("div"));
