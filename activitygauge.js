118fce036e5870c8f7b3c0eea00bac14b439e7c5
Sean Higgins <shiggins@looker.com>
2019-03-21 19:37:30 +0000

    Update activitygauge.js

--- activitygauge.js
+++ activitygauge.js
@@ -347,7 +347,7 @@ looker.plugins.visualizations.add({
                   symbolWidth: 0
                 },
 
-            series: varyseries
+            series: [varyseries]
         });
         this.trigger('registerOptions', options) // register options with parent page to update visConfig
         doneRendering()
