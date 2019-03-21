e35553edad10e3f3a24991bb4fb03b2c15c77fcc
Sean Higgins <shiggins@looker.com>
2019-03-21 23:24:15 +0000

    Update activitygauge.js

--- activitygauge.js
+++ activitygauge.js
@@ -339,7 +339,7 @@ looker.plugins.visualizations.add({
             legend: {
                   labelFormatter: function() {
                       console.log('<span style="text-weight:bold;color:' + this.color + '">' + this.name + '</span>');
-                    return this.color + this.name + this.index + this.colorIndex;
+                    return '<span style="text-weight:bold;color:' + config.color_ + this.index + '">' + this.name + '</span>';
                   },
                   enabled: config.legendtoggle,
                   symbolWidth: 0
