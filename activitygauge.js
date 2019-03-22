d0afae1e2fa586c46c64f3b6abf05136c7cf80fe
Sean Higgins <shiggins@looker.com>
2019-03-22 21:07:35 +0000

    Update activitygauge.js

--- activitygauge.js
+++ activitygauge.js
@@ -339,7 +339,7 @@ looker.plugins.visualizations.add({
             legend: {
                   labelFormatter: function() {
                     console.log('<span style="text-weight:bold;color:' + this.data[0].color + '">' + this.name + '</span>');
-                    return '<span style="text-weight:bold;color:' + this.data[0].color + '">' + this.name + '</span>';
+                    return '<span style="text-weight:bold;color:' + this.data[0].color + '">' + this.data[0].color  +this.name + '</span>';
                   },
                   enabled: config.legendtoggle,
                   symbolWidth: 0
