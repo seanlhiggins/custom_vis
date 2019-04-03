ecc54d709e36a2e1dcbe7253a9b520644414dc44
Sean Higgins <shiggins@looker.com>
2019-04-03 18:43:45 +0100

    Update grouped_stack.js

--- grouped_stack.js
+++ grouped_stack.js
@@ -58,6 +58,7 @@
             console.log(queryResponse.fields.measure_like[0].name);
             console.log(firstnrows[0][queryResponse.fields.measure_like[0].name][pivot_list[0]]);
             console.log(queryResponse)
+            console.log(data)
             // Needed to create 2 arrays from each of the pivots
             var firstPivotedMeasArray = [];
             for(let i=0;i<numDimensions;i++){
