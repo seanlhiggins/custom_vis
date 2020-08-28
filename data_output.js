f72b2d62bf3a73d9bc9309725c58f60c019dc56d
Sean Higgins <shiggins@looker.com>
2020-08-28 12:25:58 +0100

    Update data_output.js

--- data_output.js
+++ data_output.js
@@ -1,9 +1,13 @@
 
 const visObject = {
+	create: function(element, config){
+	element.innerHTML = "<h1>Ready to render!</h1>";
+},
 	updateAsync: function(data, element, config, queryResponse, details, doneRendering){
     // set the dimensions and margins of the graph
     console.log(data);
     console.log(queryResponse);
+    element.innerHTML=JSON.stringify(data)
 	}
 };
 
