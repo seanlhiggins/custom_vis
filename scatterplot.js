d1337cc81a07881be2ad1a7f4ab37b42209e51b4
Sean Higgins <shiggins@looker.com>
2020-10-07 18:51:54 +0100

    remove comments

--- scatterplot.js
+++ scatterplot.js
@@ -1,6 +1,4 @@
 /**
- * Welcome to the Looker Visualization Builder! Please refer to the following resources 
- * to help you write your visualization:
  *  - API Documentation - https://github.com/looker/custom_visualizations_v2/blob/master/docs/api_reference.md
  *  - Example Visualizations - https://github.com/looker/custom_visualizations_v2/tree/master/src/examples
  **/
